52f835eb71a8b8c5ee8398b6a27bc570d0256905
nali <nali@enst.fr>
2017-06-19 13:47:24 +0200

    gestion de timeout

--- src/Master.java
+++ src/Master.java
@@ -1,60 +1,85 @@
 import java.io.*;
-import java.util.*;
 import java.util.concurrent.*;
+import java.util.Scanner;
 
-// This program is based on the method given here: 
-// https://stackoverflow.com/questions/14165517/processbuilder-forwarding-stdout-and-stderr-of-started-processes-without-blocki
-// and this page:
-// http://tutorials.jenkov.com/java-util-concurrent/blockingqueue.html
-
-public class Master {
-
-	public static void main(String args[]) throws IOException {
-		
-		BlockingQueue queue = new ArrayBlockingQueue(2);
-		
-        String [] command = {"java", "-jar", "/tmp/slave.jar"};
-        ProcessBuilder pb = new ProcessBuilder(command);
-        
-	    Process p = pb.start();
-	    
-	    inheritIO(p.getInputStream(), System.out, queue);
-	    inheritIO(p.getErrorStream(), System.err, queue);
-	    
-	    try {
-			if (queue.poll(12, TimeUnit.SECONDS) == null) {
-				System.out.println("timeout");
-			};
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-	}
+public class Master
+{
+  public Master() {}
+  
+  public static void main(String[] paramArrayOfString) throws IOException
+  {
+	final ArrayBlockingQueue<String> InputQueue = new ArrayBlockingQueue<String>(1024);
 	
-	private static void inheritIO(final InputStream src, final PrintStream dest, final BlockingQueue queue) {
-	    new Thread(
-	    	new Runnable() {
+	String[] cmd = { "java", "-jar", "/tmp/slave.jar" };
+	ProcessBuilder process = new ProcessBuilder(cmd);
+	
+	Process p = process.start();
+	
+	/*
+	final InputStream src = p.getInputStream();
+	final PrintStream dest = System.out;
+	
+	Thread InputStreamThread = new Thread(
+			new Runnable() {
 		        public void run() {		     	
 					
 		            Scanner sc = new Scanner(src);
+		            
 		            while (sc.hasNextLine()) {
 		            	String h = sc.nextLine();
 		                dest.println(h);
-						try {
-							queue.put(h);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
+						InputQueue.add(h);
 		            }
-					
-					try {
-						queue.take();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}	
 		            
+		            sc.close();
 		        }
-	    	}
-	    ).start();
+	    	});
+	
+	InputStreamThread.start();
+	
+	try
+	{
+	  if (InputQueue.poll(2, TimeUnit.SECONDS) == null) {
+	    System.err.println("timeout(2s)");
+	    InputStreamThread.stop();
+	  }
+	} catch (InterruptedException e) {
+	  e.printStackTrace();
 	}
+	*/
+	
+	final ArrayBlockingQueue<String> ErrorQueue = new ArrayBlockingQueue<String>(1024);
+
+	
+	final InputStream src_err = p.getInputStream();
+	final PrintStream dest_err = System.out;
+	
+	Thread ErrorStreamThread = new Thread(
+			new Runnable() {
+		        public void run() {		     	
+					
+		            Scanner sc_err = new Scanner(src_err);
+		            
+		            while (sc_err.hasNextLine()) {
+		            	String h = sc_err.nextLine();
+		            	dest_err.println(h);
+		            	ErrorQueue.add(h);
+		            }
+		            
+		            sc_err.close();		    
+		        }
+	    	});
+	
+	ErrorStreamThread.start();
+	
+	try
+	{
+	  if (ErrorQueue.poll(2, TimeUnit.SECONDS) == null) {
+	    System.err.println("timeout(2s)");
+	    p.destroy();
+	  }
+	} catch (InterruptedException e) {
+	  e.printStackTrace();
+	}
+  }
 }
--- src/Slave.java
+++ src/Slave.java
@@ -3,7 +3,7 @@ public class Slave {
 	public static void main(String[] args) throws Exception {
 		
 		Thread.sleep(10000);
-		System.out.print(3+5);		
+		System.err.print(3+5);		
 
 	}
 }
